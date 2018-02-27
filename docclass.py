ca98a1398c5ef7585f8e426a600da0d62fe982b9
Chengxin Cai <chengxin.cai@gmail.com>
2018-02-27 19:08:49 +0800

    finish docclass.py

--- docclass.py
+++ docclass.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import math
 import re
 
 
@@ -124,6 +125,68 @@ class Naivebayes(Classifier):
                 continue
             if probs[cat] * self.getthreshold(best) > probs[best]:
                 return default
+
+        return best
+
+
+class FisherClassifier(Classifier):
+    def __init__(self, getfeatures):
+        super().__init__(getfeatures)
+        self.minimums = defaultdict(int)
+
+    def setminimum(self, cat, min):
+        self.minimums[cat] = min
+
+    def getminimum(self, cat):
+        return self.minimums[cat]
+
+    def cprob(self, f, cat):
+        # The frequency of this feature in this category
+        clf = self.fprob(f, cat)
+        if clf == 0:
+            return 0
+
+        # The frequency of this feature in all the categories
+        freqsum = sum(self.fprob(f, c) for c in self.categories())
+
+        # The probability is the frequency in this category divided by
+        # the overall frequency
+        p = clf / freqsum
+
+        return p
+
+    def fisherprob(self, item, cat):
+        # Multiply all the probabilities together
+        p = 1
+        features = self.getfeatures(item)
+        for f in features:
+            p *= self.weightedprob(f, cat, self.cprob)
+
+        # Take the natural log and multiply by -2
+        fscore = -2 * math.log(p)
+
+        # Use the inverse chi2 function to get a probability
+        return self.invchi2(fscore, len(features)*2)
+
+    def invchi2(self, chi, df):
+        m = chi / 2.0
+        sum = term = math.exp(-m)
+        for i in range(1, df // 2):
+            term *= m/i
+            sum += term
+
+        return min(sum, 1.0)
+
+    def classify(self, item, default=None):
+        # Loop through looking for the best result
+        best = default
+        max = 0
+        for c in self.categories():
+            p = self.fisherprob(item, c)
+            # Make sure it exceeds it's minumum
+            if p > self.getminimum(c) and p > max:
+                best = c
+                max = p
 
         return best
 
